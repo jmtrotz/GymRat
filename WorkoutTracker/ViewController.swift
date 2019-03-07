f0c2dfdf494cd8c4634bf7df00c23992413d0985
Jeffery Trotz <jeff@Jeffreys-iMac.local>
2019-03-07 13:58:16 -0500

    Initial Commit

---
+++ WorkoutTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WorkoutTracker
+//
+//  Created by Jeffery Trotz on 3/7/19.
+//  Copyright © 2019 Jeffery Trotz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
